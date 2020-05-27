578d0b2e5014d8c248522e84d5bc6e87545d3772
janecyyu <janecyyu@gmail.com>
2020-05-27 10:35:17 -0700

    change userid to username on reminder and project

--- database/seeds/003_projects.js
+++ database/seeds/003_projects.js
@@ -1,17 +1,17 @@
 exports.seed = function (knex, Promise) {
   return knex("projects").insert([
     {
-      user_id: 1,
+      my_username: "Sam",
       projectName: "Adding data to the database",
       projectType: "Needs My Feedback",
     },
     {
-      user_id: 1,
+      my_username: "Ken",
       projectName: "Data Types in SQL",
       projectType: "Dur from student",
     },
     {
-      user_id: 1,
+      my_username: "Wendy",
       projectName: "The CREATE TABLE statement",
       projectType: "Needs My Review",
     },
