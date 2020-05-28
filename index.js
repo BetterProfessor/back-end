602a2111a3626ec352b46e2a83b77a2dc76856cc
janecyyu <janecyyu@gmail.com>
2020-05-28 16:22:29 -0700

    add semail sender func

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const server = require("./api/server.js");
 require("dotenv").config();
+const emailSender = require("./emailSender");
 
 const PORT = process.env.PORT || 3333;
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
+  setInterval(emailSender.emailSender, 1500);
 });
